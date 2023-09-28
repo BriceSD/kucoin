d3d7d03a6899f2ffde4e420fd1820a663cb3809d
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-09-28 19:03:09 +0200

    feat(route): parse pair input in domain object

--- src/routes/delta.ts
+++ src/routes/delta.ts
@@ -6,5 +6,23 @@ router.get('/', (req: Request, res: Response) => {
     res.status(200).send('');
 });
 router.get('/:pair', async (req: Request, res: Response) => {
+    const rawTokens = req.params.pair.split('-');
+    if (rawTokens.length != 2) {
+        res.status(400).send('Exactly 2 symbols are required');
+    }
+    try {
+        let base = Token.parse(rawTokens[0]);
+        let quote = Token.parse(rawTokens[1]);
+        let pair = Pair.from(base, quote);
+    } catch (error) {
+        if (error instanceof TokenParseError) {
+            console.log(error.getErrorMessages());
+            res.status(400).send(error.message);
+        } else if (error instanceof PairCreationError) {
+            console.log(error.getErrorMessages());
+            res.status(400).send(error.message);
+        }
+        else {
             res.status(500).send("Something went wrong");
+        }
 });
